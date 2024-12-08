7e53e06afbab824db7ded602720985602ae12f91
joaquin <joaquinllanosg@gmail.com>
2024-12-08 14:33:57 -0300

    Se han agregado más funcionalidades al script para permitir la descarga de más tipos de archivos(ppt,word,docx,etc.). Se eliminó el contenido de la carpeta descargas que venia siendo los pdfs descargados de un curso.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// FREE SOFTWARE FOR EDUCATIONAL PURPOSES ONLY, FREE TO USE, MODIFY AND DISTRIBUTE, JUST KEEP THE ORIGINAL AUTHOR AND LICENSE.
-// Author: Joaquin LLanos
-
-
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const fs = require('fs');
@@ -9,11 +5,16 @@ const path = require('path');
 const readline = require('readline');
 
 const { promises, constants, existsSync, mkdirSync } = fs;
-const { resolve, join } = path;
+const { resolve, join, extname } = path; // Asegúrate de incluir extname aquí
 const { createInterface } = readline;
 
-const USERNAME = '##########';
-const PASSWORD = '##########';
+// Configuración
+const CONFIG = {
+    USERNAME: '#',
+    PASSWORD: '#',
+    BROWSER_PATH: 'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe',
+    DOWNLOAD_PATH: resolve(__dirname, 'descargas')
+};
 
 const rl = createInterface({
     input: process.stdin,
@@ -24,64 +25,60 @@ function sanitizeFilename(filename) {
     return filename.replace(/[<>:"/\\|?*]+/g, '').trim();
 }
 
-async function downloadImage(page, url, fullpath) {
-    const data = await page.evaluate(
-        async ({ url }) => {
-            function readAsBinaryStringAsync(blob) {
-                return new Promise((resolve, reject) => {
-                    const fr = new FileReader();
-                    fr.readAsBinaryString(blob);
-                    fr.onload = () => resolve(fr.result);
-                    fr.onerror = reject;
-                });
-            }
-
-            const r = await fetch(url, {
-                credentials: 'include',
-                headers: {
-                    accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                    'cache-control': 'no-cache',
-                    pragma: 'no-cache',
-                    'sec-fetch-mode': 'navigate',
-                    'sec-fetch-site': 'same-site',
-                    'upgrade-insecure-requests': '1'
-                },
-                referrerPolicy: 'no-referrer-when-downgrade',
-                method: 'GET',
-                mode: 'cors'
+async function downloadFile(page, url, fullpath) {
+    const data = await page.evaluate(async ({ url }) => {
+        function readAsBinaryStringAsync(blob) {
+            return new Promise((resolve, reject) => {
+                const fr = new FileReader();
+                fr.readAsBinaryString(blob);
+                fr.onload = () => resolve(fr.result);
+                fr.onerror = reject;
             });
+        }
 
-            return await readAsBinaryStringAsync(await r.blob());
-        },
-        { url }
-    );
+        const response = await fetch(url, {
+            credentials: 'include',
+            headers: {
+                accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'cache-control': 'no-cache',
+                pragma: 'no-cache',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-site',
+                'upgrade-insecure-requests': '1'
+            },
+            referrerPolicy: 'no-referrer-when-downgrade',
+            method: 'GET',
+            mode: 'cors'
+        });
+
+        return await readAsBinaryStringAsync(await response.blob());
+    }, { url });
 
     fs.writeFileSync(fullpath, data, { encoding: 'binary' });
 }
 
 const question = (query) => new Promise((resolve) => rl.question(query, resolve));
 
-// Add the stealth plugin to the puppeteer instance
+// Añadir el plugin de stealth a la instancia de puppeteer
 puppeteer.use(StealthPlugin());
 
 (async () => {
-    const downloadPath = resolve(__dirname, 'descargas');
-    if (!existsSync(downloadPath)) {
-        mkdirSync(downloadPath, { recursive: true });
+    if (!existsSync(CONFIG.DOWNLOAD_PATH)) {
+        mkdirSync(CONFIG.DOWNLOAD_PATH, { recursive: true });
     }
 
     const browser = await puppeteer.launch({
-        headless: false, // Change to true if you want to run without a graphical interface
-        executablePath: '/usr/bin/brave-browser'
+        headless: false,
+        executablePath: CONFIG.BROWSER_PATH
     });
 
     try {
         let page = await browser.newPage();
         console.log('Iniciando sesión en nave14...');
-        await page.goto('https://nave14.ucv.cl/');
+        await page.goto('https://navegador.pucv.cl');
 
-        await page.type('#rut_num', USERNAME);
-        await page.type('#user_pas', PASSWORD);
+        await page.type('#rut_num', CONFIG.USERNAME);
+        await page.type('#user_pas', CONFIG.PASSWORD);
 
         await Promise.all([
             page.click('.btn-primary[type="submit"]'),
@@ -94,58 +91,52 @@ puppeteer.use(StealthPlugin());
         const pages = await browser.pages();
         page = pages[pages.length - 1];
 
-        try {
-            await page.waitForNavigation({ waitUntil: 'networkidle0', timeout: 2000 });
-        } catch (error) {
-            console.error('Error esperando la navegación de la página:', error.message);
-        }
-
-        const pdfLinks = await page.evaluate(() => {
-            return Array.from(document.querySelectorAll('a'))
-                .map(a => ({ href: a.href, text: a.textContent.trim() }))
-                .filter(({ href }) => href && href.includes('resource') || href.includes('url'));
+        const resourceLinks = await page.evaluate(() => {
+            const links = Array.from(document.querySelectorAll('a')).map(a => ({
+                index: a.id.trim(),
+                href: a.href,
+                text: a.textContent.trim()
+            }));
+            const uniqueLinks = [...new Set(links.map(link => JSON.stringify(link)))].map(item => JSON.parse(item));
+            return uniqueLinks.filter(({ href, text }) => href && href.includes('resource') && !text.includes('File'));
         });
-        console.log(pdfLinks);
-        if (pdfLinks.length > 0) {
-            console.log(`Encontrado(s) ${pdfLinks.length} enlace(s) PDF:`);
 
-            for (const [index, pdfLink] of pdfLinks.entries()) {
-                console.log(`Procesando ${index + 1}/${pdfLinks.length}: ${pdfLink.text}`);
+        console.log(resourceLinks);
+
+        if (resourceLinks.length > 0) {
+            console.log(`Encontrado(s) ${resourceLinks.length} enlace(s) de recursos:`);
+
+            for (const [index, resourceLink] of resourceLinks.entries()) {
+                console.log(`Procesando ${index + 1}/${resourceLinks.length}: ${resourceLink.text}`);
 
                 let newPage;
                 try {
                     newPage = await browser.newPage();
-                    const client = await newPage.target().createCDPSession();
+                    console.log('waiting chrome devtools protocols session creation');
+                    const client = await newPage.createCDPSession();
                     await client.send('Page.setDownloadBehavior', {
-                        headless: true,
                         behavior: 'allow',
                         downloadPath: join(__dirname, 'descargas')
                     });
-                    await newPage.goto(pdfLink.href, { waitUntil: 'networkidle0' });
-                    await newPage.waitForSelector('a[href*=".pdf"]', { timeout: 1500 });
-                    const pdfUrl = await newPage.$eval('a[href*=".pdf"]', el => el.href);
-                    if (pdfUrl) {
-                        let fileName = `${pdfLink.text}.pdf`;
+                    console.log('processing link in goto');
+                    await newPage.goto(resourceLink.href, { waitUntil: 'networkidle0' });
+                    console.log('no error in goto func');
+                    await newPage.waitForSelector('a[href*=".pdf"], a[href*=".pptx"], a[href*=".docx"], a[href*=".sqlite"], a[href*=".sql"], a[href*=".ipynb"]', { timeout: 1000 });
+                    console.log('no error in selector func');
+                    const resourceUrl = await newPage.$eval('a[href*=".pdf"], a[href*=".pptx"], a[href*=".docx"], a[href*=".sqlite"], a[href*=".sql"], a[href*=".ipynb"]', el => el.href).catch(() => null);
+                    if (resourceUrl) {
+                        const extension = extname(resourceUrl);
+                        let fileName = `${resourceLink.text}${extension}`;
                         fileName = sanitizeFilename(fileName);
-                        await downloadImage(newPage, pdfUrl, join(__dirname, 'descargas', fileName));
-                        console.log(`PDF descargado: ${fileName}`);
+                        await downloadFile(newPage, resourceUrl, join(__dirname, 'descargas', fileName));
+                        console.log(`Recurso descargado: ${fileName}`);
                         console.log(`Ubicación: ${join(__dirname, 'descargas', fileName)}`);
-                        console.log('link:', pdfUrl, '\n');
                     } else {
-                        console.log(`No se pudo encontrar el enlace del PDF para: ${pdfLink.text}`);
+                        console.log(`No se pudo encontrar el enlace del recurso para: ${resourceLink.text}`);
                     }
-                    await newPage.waitForSelector('a[href*="htmlpresent"]', { timeout: 1500 });
-                    const pptUrl = await newPage.$eval('a[href*="htmlpresent"]', el => el.href);
-                    if (pptUrl) {
-                        let fileName = `${pdfLink.text}.pptx`;
-                        fileName = sanitizeFilename(fileName);
-                        await downloadImage(newPage, pptUrl, join(__dirname, 'descargas', fileName));
-                        console.log(`PPT descargado: ${fileName}`);
-                        console.log(`Ubicación: ${join(__dirname, 'descargas', fileName)}`);
-                        console.log('link:', pdfUrl, '\n');
-                    }
+
                 } catch (error) {
-                    console.error(`Error procesando ${pdfLink.text}: ${error.message}`);
+                    console.error(`Error procesando ${resourceLink.text}: ${error.message}`);
                 } finally {
                     if (newPage && !newPage.isClosed()) {
                         await newPage.close();
@@ -153,7 +144,7 @@ puppeteer.use(StealthPlugin());
                 }
             }
         } else {
-            console.log('No se encontraron enlaces PDF en la página.');
+            console.log('No se encontraron enlaces de recursos en la página.');
         }
     } catch (error) {
         console.error('Error al procesar la página:', error);
